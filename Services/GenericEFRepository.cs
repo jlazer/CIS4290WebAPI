a995db46e12d7ea7b1d4436e8394d289e3c316cd
Justin Ng <jmng@cpp.edu>
2021-04-20 18:51:01 -0700

    Created Services

---
+++ Services/GenericEFRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Entities;
+using System.Reflection;
+
+namespace WebAPI.Services
+{
+    public class GenericEFRepository : IGenericEFRepository
+    {
+        private SqlDbContext _db;
+        public GenericEFRepository(SqlDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<TEntity> Get<TEntity>() where TEntity : class
+        {
+            return _db.Set<TEntity>();
+        }
+
+        public TEntity Get<TEntity>(int id, bool includeRelatedEntities = false) where TEntity : class
+        {
+            var entity = _db.Set<TEntity>().Find(new object[] { id });
+
+            if (entity != null && includeRelatedEntities)
+            {
+                // Get the names of all DbSets in the DbContext
+                var dbsets = typeof(SqlDbContext)
+                    .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    .Where(z => z.PropertyType.Name.Contains("DbSet"))
+                    .Select(z => z.Name);
+
+                // Get the names of all the properties (tables) in the generic
+                // type T that is represented by a DbSet
+                var tables = typeof(TEntity)
+                    .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    .Where(z => dbsets.Contains(z.Name))
+                    .Select(z => z.Name);
+
+                // Load all the tables referenced by the generic type T
+                if (tables.Count() > 0)
+                    foreach (var table in tables)
+                        _db.Entry(entity).Collection(table).Load();
+            }
+
+            return entity;
+        }
+
+        public void Add<TEntity>(TEntity item) where TEntity : class
+        {
+            _db.Add<TEntity>(item);
+        }
+
+        public void Delete<TEntity>(TEntity item) where TEntity : class
+        {
+            _db.Set<TEntity>().Remove(item);
+        }
+
+        public bool Exists<TEntity>(int id) where TEntity : class
+        {
+            return _db.Set<TEntity>().Find(new object[] { id }) != null;
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0;
+        }
+
+    }
+
+}
---
+++ Services/IGenericEFRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Services
+{
+    public interface IGenericEFRepository
+    {
+        IEnumerable<TEntity> Get<TEntity>() where TEntity : class;
+        TEntity Get<TEntity>(int id, bool includeRelatedEntities = false) where TEntity : class;
+        void Add<TEntity>(TEntity item) where TEntity : class;
+        void Delete<TEntity>(TEntity item) where TEntity : class;
+        bool Exists<TEntity>(int id) where TEntity : class;
+        bool Save();
+    }
+
+}
