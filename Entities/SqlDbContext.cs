a9f52f8715382afa9422ed66175e48e3277a5a2c
Jeremiah David <jeremiahd@cpp.edu>
2021-04-28 16:29:41 -0700

    Review requests are fixed!

--- Entities/SqlDbContext.cs
+++ Entities/SqlDbContext.cs
@@ -8,6 +8,7 @@ namespace WebAPI.Entities
         public DbSet<Cart> Cart { get; set; }
         public DbSet<Product> Product { get; set; }
         public DbSet<Review> Review { get; set; }
+        public DbSet<Category> Category { get; set; }
 
         public SqlDbContext(DbContextOptions<SqlDbContext> options)
         : base(options)
